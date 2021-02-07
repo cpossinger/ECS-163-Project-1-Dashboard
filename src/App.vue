e41608ba073419a26575bcc2374ca6b49a2494c6
cpossinger <cposs2000@gmail.com>
2021-02-06 21:33:11 -0800

    Added Data

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,34 @@
 <!-- This file will be used to layout and connect your views -->
 <template>
-  <div id="app">
-      <!-- This is only a basic setup to display your views. Use HTML and CSS to create your layout using these basic commands -->
-      <View1 />
-      <View2 />
-      <View3 />
-  </div>
+  <v-app>
+    <v-app-bar app></v-app-bar>
+    <v-btn @click="toggleTheme" text rounded> Toggle Theme</v-btn>
+    <v-main>
+      <v-container>
+        Hello World
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 // Try to use descriptive names like 'OverviewScatterPlot' or 'DetailLineChart'
-import View1 from './components/View1.vue'
-import View2 from './components/View2.vue'
-import View3 from './components/View3.vue'
+//import View1 from './components/View1.vue'
+//import View2 from './components/View2.vue'
+//import View3 from './components/View3.vue'
 
 export default {
   name: 'App',
   components: {
-    View1,
-    View2,
-    View3
+//    View1,
+ //   View2,
+  //  View3
+  },
+  methods: {
+    toggleTheme(){
+      this.$vuetify.theme.themes.dark.anchor = "#41B883"
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+    }
   }
 }
 </script>
